2f24e76f90c154abc4f1b6397e69c842fc5b8c65
Peace Elisha Iratishe <peaceelishairatishe@gmail.com>
2025-11-28 15:27:41 +0100

    Create Models

---
+++ event-sheduler-and-conflict-detector/event-sheduler-and-conflict-detector-api/Controllers/EventController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace event_sheduler_and_conflict_detector_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+    }
+}
---
+++ event-sheduler-and-conflict-detector/event-sheduler-and-conflict-detector-api/Models/Entities/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace event_sheduler_and_conflict_detector_api.Models.Entities
+{
+    public class Event
+    {
+        public required string Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public required string Location { get; set;
+        public required string Attendees  { get; set; }
+        public string? EventType  { get; set; }
+
+    }
+}
