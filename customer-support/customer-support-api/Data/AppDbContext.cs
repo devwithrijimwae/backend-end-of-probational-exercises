adf7e6d2e5856cd44718bcd0ec71228a7fe0990e
Peace Elisha Iratishe <peaceelishairatishe@gmail.com>
2025-12-01 11:47:53 +0100

    Implement DbContext for CRUD Operation

---
+++ customer-support/customer-support-api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using customer_support_api.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace customer_support_api.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions <AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
